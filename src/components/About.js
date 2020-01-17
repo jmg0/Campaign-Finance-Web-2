f266ec6bb20bc62ed89b8cf3644394f1e400e017
James Garay <jmgaray11@gmail.com>
2020-01-17 10:29:32 -0500

    updated footer and added shell of about page albeit less than optimally

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <div>
+            <h1>HELLO PLANET EARTH</h1>
+        </div>
+    )
+};
+
+export default About;
--- src/containers/App.js
+++ src/containers/App.js
@@ -8,6 +8,7 @@ import {candidates} from "../candidates";
 import BackButton from "../components/BackButton";
 import CandidateMap from "../components/CandidateMap";
 import ReactTooltip from 'react-tooltip'
+import About from "../components/About";
 
 const particlesOptions = {
   particles: {
@@ -83,8 +84,10 @@ class App extends Component {
                             </div>
                             <div className='row1'>
                               <h2 className='slightlybigger underline' data-tip data-for='conts'>Individual Contributions</h2>
-                              <ReactTooltip id='conts' place="top" type="dark" effect="solid">
-                                <p>All contributions from individuals to the campaign in 2019.</p>
+                              <ReactTooltip id='conts' place="right" type="dark" effect="solid">
+                                <p>All contributions from individuals to the {candidateNames[route]} campaign in 2019 are detailed below.</p>
+                                <p>These exclude certain contributions to PACs and other committees that are not subject to reporting.</p>
+                                <p>See About page for more details.</p>
                               </ReactTooltip>
                             </div>
                             <div className='row2'>
@@ -94,16 +97,27 @@ class App extends Component {
                             <div className='row5'>
                               <div className='i1'>
                                 <h4 className='sub1' data-tip data-for='itemized'>Itemized - {candidates[route].itemizedTotal}</h4>
-                                <ReactTooltip id='itemized' place="top" type="dark" effect="solid">All individual contributions reported by the campaign.</ReactTooltip>
+                                <ReactTooltip id='itemized' place="right" type="dark" effect="solid">
+                                  <p>All individual contributions reported by the campaign.</p>
+                                </ReactTooltip>
                               </div>
                               <div className='i2'>
                                 <h4 className='sub2' data-tip data-for='unitemized'>Unitemized - {candidates[route].nonItemizedTotal}</h4>
-                                <ReactTooltip id='unitemized' place="top" type="dark" effect="solid">Campaigns are only required to report contributions over $200.00. Smaller contributions must be recorded and tabulated, but can be reported as a lump sum of unitemized contributions.</ReactTooltip>
+                                <ReactTooltip id='unitemized' place="right" type="dark" effect="solid">
+                                  <p>Campaigns are only required to report contributions over $200.00.</p>
+                                  <p>Smaller contributions must be recorded and tabulated, but can be reported as a single sum of unitemized contributions.</p>
+                                  <p>Contributions recorded in this category contain no information about the contributors, so they are not displayed on the map.</p>
+                                  <p>See About page for more details.</p>
+                                </ReactTooltip>
                               </div>
                             </div>
                             <div className='row6'>
                               <p></p>
-                              <h2>Contributors - {candidates[route].numContributors}</h2>
+                              <h2 data-tip data-for='contributors'>Contributors - {candidates[route].numContributors}</h2>
+                              <ReactTooltip id='contributors' place="right" type="dark" effect="solid">
+                                <p>The number of unique individuals who contributed to the {candidateNames[route]} campaign in 2019.</p>
+                                <p>This only includes itemized contributors, as no contributor information is reported for unitemized contributions.</p>
+                              </ReactTooltip>
                             </div>
                           </div>
                           <div className='item2'>
@@ -112,20 +126,25 @@ class App extends Component {
                         </div>
                       </div>
                       :
-                      <div>
-                        <BackButton onBackButtonChange={this.onBackButtonChange} />
-                        <h1>Sorry no page here</h1>
-                        {console.log(route)}
-                      </div>
+                          <div>
+                            <BackButton onBackButtonChange={this.onBackButtonChange} />
+                            <About />
+                          </div>
           }
           <footer>
             <div className='justify-start'>
-              <h5>Created by James Garay</h5>
-              <a href='https://github.com/jmg0/Campaign-Finance-Analysis'>
-                <svg className='octicon' height='24px' width='16px'>
-                  <path fillRule='evenodd' d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-              </a>
+              <div className='inline-flex'>
+                <h5 className='pointer' onClick={this.onRouteChange}>About </h5>
+                <h5 className='demarc'>*</h5>
+                <h5>Created by James Garay</h5>
+              </div>
+              <div>
+                <a href='https://github.com/jmg0/Campaign-Finance-Analysis'>
+                  <svg className='octicon' height='24px' width='16px'>
+                    <path fillRule='evenodd' d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </footer>
         </div>
